399c76c3575d78595ab60a0d0bbbd3fdfb41d5bf
CharanBS18 <charan201204@gmail.com>
2026-03-21 18:32:17 +0530

    Hardcode repository absolute paths

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Forces relative pathing for Github Pages compatibility
+  base: '/carwebsite/', // Set explicitly to repo name to avoid trailing slash bugs
 })
